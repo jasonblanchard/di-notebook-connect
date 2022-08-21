71c974aeedacd20d3da812e62d8421df8a11754a
jasonblanchard <jas.blanchard@gmail.com>
2022-08-21 15:55:52 -0400

    env

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/bufbuild/connect-go"
 	"github.com/jasonblanchard/di-notebook-connect/gen/proto/go/notebookapis/notebook/v1/notebookv1connect"
@@ -15,13 +17,24 @@ import (
 	notebookstore "github.com/jasonblanchard/di-notebook-connect/gen/sqlc/notebook"
 	notebookv1 "github.com/jasonblanchard/di-notebook-connect/services/notebook/v1"
 	pingv1 "github.com/jasonblanchard/di-notebook-connect/services/ping/v1"
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 )
 
 func main() {
-	db, err := sql.Open("postgres", "user=postgres host=localhost port=52926 dbname=postgres password=mysecretpassword sslmode=disable")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	pghost := os.Getenv("PGHOST")
+	pgport := os.Getenv("PGPORT")
+	pgdatabase := os.Getenv("PGDATABASE")
+	pgpassword := os.Getenv("PGPASSWORD")
+
+	db, err := sql.Open("postgres", fmt.Sprintf("user=postgres host=%s port=%s dbname=%s password=%s sslmode=disable", pghost, pgport, pgdatabase, pgpassword))
 	if err != nil {
 		panic(err)
 	}
@@ -45,18 +58,19 @@ func main() {
 		Logger: sugaredLogger,
 	}
 
-	// TODO: Interceptor for translating `Authorization: Bearer aaa.bbb.ccc` JWT to x-principal-id https://connect.build/docs/go/interceptors
 	interceptors := connect.WithInterceptors(ingress.NewAuthInterceptor())
 
 	notebookpath, notebookhandler := notebookv1connect.NewNotebookServiceHandler(notebookService, interceptors)
 	mux.Handle(notebookpath, notebookhandler)
 
-	fmt.Println("Starting server on port 8080")
+	port := os.Getenv("PORT")
+
+	fmt.Printf("Starting server on port %s\n", port)
 
 	wrappedMux := NewWithLogger(mux, sugaredLogger)
 
 	http.ListenAndServe(
-		"localhost:8080",
+		fmt.Sprintf("0.0.0.0:%s", port),
 		// Use h2c so we can serve HTTP/2 without TLS.
 		h2c.NewHandler(wrappedMux, &http2.Server{}),
 	)
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/bufbuild/connect-go v0.2.0
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
+	github.com/joho/godotenv v1.4.0
 	github.com/lib/pq v1.10.6
 	go.uber.org/zap v1.22.0
 	golang.org/x/net v0.0.0-20220722155237-a158d28d115b
